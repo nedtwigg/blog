d4bf59ce9a4da77e508ac163ea37842fdbd9c0bc
Ned Twigg <ned.twigg@diffplug.com>
2020-01-03 11:13:27 -0800

    Improved the image-smashing.

--- 2020/changelog/imagegen.m
+++ 2020/changelog/imagegen.m
@@ -4,38 +4,42 @@ figure('Name', 'Original')
 imshow(rgb)
 unit = ones(size(rgb,1), size(rgb,2));
 
+figure('Name', 'Smashed')
+
 %% 0.(r+g).b
 smashed_0_rg_b = cat(3, 0.*unit, rgb(:,:,1) + rgb(:,:,2), rgb(:,:,3));
-figure('Name', '0.(r+g).b')
+subplot(2,2,1)
 imshow(smashed_0_rg_b)
+title('0.(r+g).b')
+
 
 %% 0.(r+g)/2.b
 smashed_0_rg2_b = cat(3, 0.*unit, 0.5.*(rgb(:,:,1) + rgb(:,:,2)), rgb(:,:,3));
-figure('Name', '0.(r+g)/2.b')
+subplot(2,2,2)
 imshow(smashed_0_rg2_b)
+title('0.(r+g)/2.b')
 
 %% YCbCr
-ycbcr = toYCbCr(rgb);
-rgb_round = fromYCbCr(ycbcr);
-figure('Name', 'Y.Cb.Cr')
-imshow(rgb_round)
+r = double(rgb(:,:,1));
+g = double(rgb(:,:,2));
+b = double(rgb(:,:,3));
+unit = ones(size(rgb,1), size(rgb,2));
+y = limitRound(            0.299   .*r + 0.587   .*g + 0.114   .*b);
+cb = limitRound(128*unit - 0.168736.*r - 0.331264.*g + 0.5     .*b);
+cr = limitRound(128*unit + 0.5     .*r - 0.418688.*g - 0.081312.*b);
+
+subplot(2,2,3)
+imshow(fromYCbCr(cat(3, 0*unit, y+cb, cr)))
+title('0.(Y+Cb).Cr')
+
+subplot(2,2,4)
+imshow(fromYCbCr(cat(3, 0*unit, 0.5.*(y+cb), cr)))
+title('0.(Y+Cb)/2.Cr')
 
 %% Math
 function out = limitRound(in)
     out = uint8(max(0, min(255, round(in))));
 end
-
-function out = toYCbCr(rgb)
-    r = double(rgb(:,:,1));
-    g = double(rgb(:,:,2));
-    b = double(rgb(:,:,3));
-    unit = ones(size(rgb,1), size(rgb,2));
-	y = limitRound(            0.299   .*r + 0.587   .*g + 0.114   .*b);
-	cb = limitRound(128*unit - 0.168736.*r - 0.331264.*g + 0.5     .*b);
-	cr = limitRound(128*unit + 0.5     .*r - 0.418688.*g - 0.081312.*b);
-	out = cat(3, y, cb, cr);
-end
-
 
 function out = fromYCbCr(ycbcr)
     y  = double(ycbcr(:,:,1));
