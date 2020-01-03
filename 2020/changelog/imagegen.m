eb88182f0e108666c52fe8f6cbd80f6f86e7496c
Ned Twigg <ned.twigg@diffplug.com>
2020-01-03 11:26:33 -0800

    Added view which scales only the saturated channel.

--- 2020/changelog/imagegen.m
+++ 2020/changelog/imagegen.m
@@ -8,14 +8,14 @@ figure('Name', 'Smashed')
 
 %% 0.(r+g).b
 smashed_0_rg_b = cat(3, 0.*unit, rgb(:,:,1) + rgb(:,:,2), rgb(:,:,3));
-subplot(2,2,1)
+subplot(3,2,1)
 imshow(smashed_0_rg_b)
 title('0.(r+g).b')
 
 
 %% 0.(r+g)/2.b
 smashed_0_rg2_b = cat(3, 0.*unit, 0.5.*(rgb(:,:,1) + rgb(:,:,2)), rgb(:,:,3));
-subplot(2,2,2)
+subplot(3,2,2)
 imshow(smashed_0_rg2_b)
 title('0.(r+g)/2.b')
 
@@ -28,13 +28,34 @@ y = limitRound(            0.299   .*r + 0.587   .*g + 0.114   .*b);
 cb = limitRound(128*unit - 0.168736.*r - 0.331264.*g + 0.5     .*b);
 cr = limitRound(128*unit + 0.5     .*r - 0.418688.*g - 0.081312.*b);
 
-subplot(2,2,3)
+subplot(3,2,3)
 imshow(fromYCbCr(cat(3, 0*unit, y+cb, cr)))
 title('0.(Y+Cb).Cr')
 
-subplot(2,2,4)
+subplot(3,2,4)
 imshow(fromYCbCr(cat(3, 0*unit, 0.5.*(y+cb), cr)))
 title('0.(Y+Cb)/2.Cr')
+
+%% Rescaled only as necessary (RGB)
+r = double(rgb(:,:,1));
+g = double(rgb(:,:,2));
+b = double(rgb(:,:,3));
+unit = ones(size(rgb,1), size(rgb,2));
+
+subplot(3,2,5)
+smashed_rg_norm = (r+g) * (255/max(max(r+g)));
+imshow(uint8(cat(3, 0*unit, smashed_rg_norm, b)))
+title('0.(r+g)^N.b')
+
+%% Rescaled only as necessary (YCbCr)
+subplot(3,2,6)
+
+y_  = double(y);
+cb_ = double(cb);
+
+smashed_ycb_norm = (y_+cb_) * (255/max(max(y_+cb_)));
+imshow(fromYCbCr(cat(3, 0*unit, smashed_ycb_norm, cr)))
+title('0.(Y+Cb)^N.Cr')
 
 %% Math
 function out = limitRound(in)
